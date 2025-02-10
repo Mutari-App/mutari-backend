2ac40bbbbaa04ce98dee7bc7b4b80bd058722a04
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2025-02-10 15:57:06 +0700

    fix: add Public decorator to static api

--- src/static/static.controller.ts
+++ src/static/static.controller.ts
@@ -7,23 +7,27 @@ import {
   Query,
 } from '@nestjs/common'
 import { StaticService } from './static.service'
+import { Public } from 'src/common/decorators/public.decorator'
 
 @Controller('static')
 export class StaticController {
   constructor(private readonly staticService: StaticService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Get('countries')
   async getCountries(@Query('query') query: string) {
     return this.staticService.searchCountries(query)
   }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Get('countries/:placeId')
   async getCountryCode(@Param('placeId') placeId: string) {
     return this.staticService.getCountryCode(placeId)
   }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Get('cities/:countryCode')
   async getCities(
