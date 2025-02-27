3d0bfd29aa0476952c3aa93306591e6f81bf1681
daffarafip24 <daffarafip24@gmail.com>
2025-02-27 14:04:57 +0700

    test: create test for login controller and login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { LoginDTO } from './dto/login.dto'
+import { ResponseUtil } from 'src/common/utils/response.util'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+    private readonly responseUtil: ResponseUtil
+  ) {}
+
+  async login(loginDto: LoginDTO) {
+    return null
+  }
+}
