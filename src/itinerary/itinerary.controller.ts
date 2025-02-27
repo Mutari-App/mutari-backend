1b1737abfb93f42a7a7263f41c6f99dcdffa7346
Fathan <fathan.naufal@ui.ac.id>
2025-02-27 22:21:49 +0700

    feat: implement findMyItinerary controller

--- src/itinerary/itinerary.controller.ts
+++ src/itinerary/itinerary.controller.ts
@@ -6,14 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  HttpStatus,
 } from '@nestjs/common'
 import { ItineraryService } from './itinerary.service'
 import { CreateItineraryDto } from './dto/create-itinerary.dto'
 import { UpdateItineraryDto } from './dto/update-itinerary.dto'
+import { GetUser } from 'src/common/decorators/getUser.decorator'
+import { PaginationDto } from './dto/pagination.dto'
+import { User } from '@prisma/client'
+import { ResponseUtil } from 'src/common/utils/response.util'
 
 @Controller('itinerary')
 export class ItineraryController {
-  constructor(private readonly itineraryService: ItineraryService) {}
+  constructor(
+    private readonly itineraryService: ItineraryService,
+    private readonly responseUtil: ResponseUtil
+  ) {}
 
   @Post()
   create(@Body() createItineraryDto: CreateItineraryDto) {
@@ -23,6 +32,26 @@ export class ItineraryController {
   @Get()
   findAll() {
     return this.itineraryService.findAll()
+  }
+
+  @Get()
+  async findMyItineraries(
+    @GetUser() user: User,
+    @Query() paginationDto: PaginationDto
+  ) {
+    const response = await this.itineraryService.findMyItineraries(
+      user.id,
+      paginationDto.page
+    )
+    return this.responseUtil.response(
+      {
+        statusCode: HttpStatus.OK,
+        message: 'Itineraries fetched successfully.',
+      },
+      {
+        response,
+      }
+    )
   }
 
   @Get(':id')
