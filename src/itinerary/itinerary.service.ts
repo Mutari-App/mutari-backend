c939c1ef7cc37040fa31a59fa7510e902b0845ef
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-03 11:11:43 +0700

    feat: implement findOne() service

--- src/itinerary/itinerary.service.ts
+++ src/itinerary/itinerary.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { CreateItineraryDto } from './dto/create-itinerary.dto'
 import { UpdateItineraryDto } from './dto/update-itinerary.dto'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class ItineraryService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createItineraryDto: CreateItineraryDto) {
     return 'This action adds a new itinerary'
   }
@@ -12,8 +15,14 @@ export class ItineraryService {
     return `This action returns all itinerary`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} itinerary`
+  async findOne(id: string) {
+    const itinerary = await this.prisma.itinerary.findUnique({
+      where: { id },
+    })
+    if (!itinerary) {
+      throw new NotFoundException(`Itinerary with ID ${id} not found`)
+    }
+    return itinerary
   }
 
   update(id: number, updateItineraryDto: UpdateItineraryDto) {
