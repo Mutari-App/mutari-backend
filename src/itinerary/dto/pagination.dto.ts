2fcd342bdb124515d0b2cc938d87cac2733b6953
Fathan <fathan.naufal@ui.ac.id>
2025-03-03 09:08:16 +0700

    refactor: add type conversion in pagination dto

--- src/itinerary/dto/pagination.dto.ts
+++ src/itinerary/dto/pagination.dto.ts
@@ -1,6 +1,8 @@
+import { Type } from 'class-transformer'
 import { IsNumber, IsOptional, IsPositive } from 'class-validator'
 
 export class PaginationDto {
+  @Type(() => Number)
   @IsNumber()
   @IsPositive()
   @IsOptional()
