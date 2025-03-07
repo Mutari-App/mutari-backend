068cde1acdc94fd0cace9e7768e74ca3b7b3b5be
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-07 13:57:04 +0700

    test: add unit tests for itinerary removal with response utility

--- src/itinerary/itinerary.controller.spec.ts
+++ src/itinerary/itinerary.controller.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ItineraryController } from './itinerary.controller'
 import { ItineraryService } from './itinerary.service'
+import { ResponseUtil } from 'src/common/utils/response.util'
+import { HttpStatus } from '@nestjs/common'
 
 describe('ItineraryController', () => {
   let controller: ItineraryController
   let service: ItineraryService
+  let responseUtil: ResponseUtil
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,11 +19,18 @@ describe('ItineraryController', () => {
             removeItinerary: jest.fn(),
           },
         },
+        {
+          provide: ResponseUtil,
+          useValue: {
+            response: jest.fn(),
+          },
+        },
       ],
     }).compile()
 
     controller = module.get<ItineraryController>(ItineraryController)
     service = module.get<ItineraryService>(ItineraryService)
+    responseUtil = module.get<ResponseUtil>(ResponseUtil)
   })
 
   it('should be defined', () => {
@@ -32,18 +42,29 @@ describe('ItineraryController', () => {
       const itineraryId = 'ITN123'
       jest.spyOn(service, 'removeItinerary').mockResolvedValue(undefined)
 
-      await controller.removeItinerary(itineraryId)
+      const responseMock = {
+        statusCode: HttpStatus.OK,
+        message: 'Itinerary deleted successfully.',
+      }
+
+      jest.spyOn(responseUtil, 'response').mockReturnValue(responseMock)
+
+      const result = await controller.removeItinerary(itineraryId)
+
       expect(service.removeItinerary).toHaveBeenCalledWith(itineraryId)
+      expect(responseUtil.response).toHaveBeenCalledWith(responseMock, null)
+      expect(result).toEqual(responseMock)
     })
     it('should throw an error if itinerary is not found', async () => {
       const itineraryId = 'non-existent-id'
-      jest
-        .spyOn(service, 'removeItinerary')
-        .mockRejectedValue(new Error('Not Found'))
+      const error = new Error('Not Found')
+
+      jest.spyOn(service, 'removeItinerary').mockRejectedValue(error)
 
       await expect(controller.removeItinerary(itineraryId)).rejects.toThrow(
         'Not Found'
       )
+
       expect(service.removeItinerary).toHaveBeenCalledWith(itineraryId)
     })
   })
