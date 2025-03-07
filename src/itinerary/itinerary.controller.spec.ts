4dd531873e28bc089547edffcd8aec563f9d8450
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-07 12:51:56 +0700

    test: update ItineraryController tests to include response utility and mock data

--- src/itinerary/itinerary.controller.spec.ts
+++ src/itinerary/itinerary.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ItineraryController } from './itinerary.controller'
 import { ItineraryService } from './itinerary.service'
-import { NotFoundException } from '@nestjs/common'
+import { HttpStatus, NotFoundException } from '@nestjs/common'
+import { ResponseUtil } from 'src/common/utils/response.util'
+import { BLOCK_TYPE, User } from '@prisma/client'
 
 describe('ItineraryController', () => {
   let controller: ItineraryController
   let service: ItineraryService
+  let responseUtil: ResponseUtil
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,11 +20,18 @@ describe('ItineraryController', () => {
             findOne: jest.fn(),
           },
         },
+        {
+          provide: ResponseUtil,
+          useValue: {
+            response: jest.fn(),
+          },
+        },
       ],
     }).compile()
 
     controller = module.get<ItineraryController>(ItineraryController)
     service = module.get<ItineraryService>(ItineraryService)
+    responseUtil = module.get<ResponseUtil>(ResponseUtil)
   })
 
   it('should be defined', () => {
@@ -43,12 +53,68 @@ describe('ItineraryController', () => {
       isCompleted: true,
     }
 
-    it('should return an itinerary if found', async () => {
-      jest.spyOn(service, 'findOne').mockResolvedValue(mockItinerary)
+    it('should return an itinerary when found', async () => {
+      const mockItinerary = {
+        id: 'ITN-123',
+        userId: 'USR-123',
+        title: 'Trip to Bali',
+        description: 'Bali with friends',
+        coverImage: 'https://example.com/image.jpg',
+        startDate: new Date(),
+        endDate: new Date(),
+        isPublished: true,
+        isCompleted: false,
+        updatedAt: new Date(),
+        createdAt: new Date(),
+        sections: [
+          {
+            id: 'section1',
+            itineraryId: 'ITN-123',
+            sectionNumber: 1,
+            updatedAt: new Date(),
+            createdAt: new Date(),
+            title: 'Section 1',
+            blocks: [
+              {
+                id: 'block1',
+                updatedAt: new Date(),
+                createdAt: new Date(),
+                title: 'Block Title',
+                description: 'Block Description',
+                sectionId: 'section1',
+                position: 1,
+                blockType: BLOCK_TYPE.LOCATION,
+                startTime: new Date(),
+                endTime: new Date(),
+                location: 'New York',
+                price: 100,
+                photoUrl: 'https://example.com/photo.jpg',
+              },
+            ],
+          },
+        ],
+      }
 
-      const result = await controller.findOne('ITN123')
-      expect(result).toEqual(mockItinerary)
-      expect(service.findOne).toHaveBeenCalledWith('ITN123')
+      const mockResponse = {
+        statusCode: HttpStatus.OK,
+        message: 'Itinerary fetched successfully.',
+        data: mockItinerary,
+      }
+
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockItinerary)
+      jest.spyOn(responseUtil, 'response').mockReturnValue(mockResponse)
+
+      const result = await controller.findOne('ITN-123')
+
+      expect(service.findOne).toHaveBeenCalledWith('ITN-123')
+      expect(responseUtil.response).toHaveBeenCalledWith(
+        {
+          statusCode: HttpStatus.OK,
+          message: 'Itinerary fetched successfully.',
+        },
+        { data: mockItinerary }
+      )
+      expect(result).toBe(mockResponse)
     })
 
     it('should throw NotFoundException if itinerary is not found', async () => {
@@ -57,8 +123,6 @@ describe('ItineraryController', () => {
       await expect(controller.findOne('INVALID_ID')).rejects.toThrow(
         new NotFoundException('Itinerary with ID INVALID_ID not found')
       )
-
-      expect(service.findOne).toHaveBeenCalledWith('INVALID_ID')
     })
   })
 })
