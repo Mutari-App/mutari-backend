4b5b47f144022c4028ed843195a5ea4b73645fb6
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-07 13:58:34 +0700

    feat: enhance itinerary removal with response utility

--- src/itinerary/itinerary.controller.ts
+++ src/itinerary/itinerary.controller.ts
@@ -6,14 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common'
 import { ItineraryService } from './itinerary.service'
 import { CreateItineraryDto } from './dto/create-itinerary.dto'
 import { UpdateItineraryDto } from './dto/update-itinerary.dto'
+import { ResponseUtil } from 'src/common/utils/response.util'
 
 @Controller('itinerary')
 export class ItineraryController {
-  constructor(private readonly itineraryService: ItineraryService) {}
+  constructor(
+    private readonly itineraryService: ItineraryService,
+    private readonly responseUtil: ResponseUtil
+  ) {}
 
   @Post()
   create(@Body() createItineraryDto: CreateItineraryDto) {
@@ -41,5 +46,12 @@ export class ItineraryController {
   @Delete(':id')
   async removeItinerary(@Param('id') id: string) {
     await this.itineraryService.removeItinerary(id)
+    return this.responseUtil.response(
+      {
+        statusCode: HttpStatus.OK,
+        message: 'Itinerary deleted successfully.',
+      },
+      null
+    )
   }
 }
