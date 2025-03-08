d3049897a9236d48b63fa279032da83d80cc6ca4
daffarafip24 <daffarafip24@gmail.com>
2025-03-08 19:41:28 +0700

    test: adjust tests to align with cookie usage in AuthController

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -4,30 +4,47 @@ import { AuthService } from './auth.service'
 import { LoginDTO } from './dto/login.dto'
 import { UnauthorizedException } from '@nestjs/common'
 import { ResponseUtil } from 'src/common/utils/response.util'
+import { Request, Response } from 'express'
+import { COOKIE_CONFIG } from './constant'
 
-describe('AuthController', () => {
+fdescribe('AuthController', () => {
   let controller: AuthController
   let service: AuthService
+  let mockResponse: Partial<Response>
+  let mockRequest: Partial<Request>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
         {
-          provide: ResponseUtil,
-          useClass: ResponseUtil,
-        },
-        {
           provide: AuthService,
           useValue: {
             login: jest.fn(),
+            refreshToken: jest.fn(),
           },
+        },
+        {
+          provide: ResponseUtil,
+          useClass: ResponseUtil,
         },
       ],
     }).compile()
 
     controller = module.get<AuthController>(AuthController)
     service = module.get<AuthService>(AuthService)
+
+    mockResponse = {
+      cookie: jest.fn(),
+    } as Partial<Response>
+
+    mockRequest = {
+      user: {
+        user: { id: 1, email: 'test@example.com' },
+        refreshTokenExpiresAt: new Date(),
+      },
+      cookies: { refreshToken: 'mocked-refresh-token' },
+    } as Partial<Request>
   })
 
   describe('login', () => {
@@ -36,24 +53,30 @@ describe('AuthController', () => {
         email: 'test@example.com',
         password: 'password123',
       }
-      const mockResponse = {
+      const mockLoginResponse = {
         accessToken: 'mocked-access-token',
         refreshToken: 'mocked-refresh-token',
       }
 
-      jest.spyOn(service, 'login').mockResolvedValue(mockResponse)
+      jest.spyOn(service, 'login').mockResolvedValue(mockLoginResponse)
 
-      const result = await controller.login(dto)
+      const result = await controller.login(dto, mockResponse as Response)
 
       expect(service.login).toHaveBeenCalledWith(dto)
+      expect(mockResponse.cookie).toHaveBeenCalledWith(
+        COOKIE_CONFIG.accessToken.name,
+        mockLoginResponse.accessToken,
+        expect.any(Object)
+      )
+      expect(mockResponse.cookie).toHaveBeenCalledWith(
+        COOKIE_CONFIG.refreshToken.name,
+        mockLoginResponse.refreshToken,
+        expect.any(Object)
+      )
       expect(result).toEqual({
-        success: true,
-        statusCode: 200,
         message: 'Success Login',
-        data: {
-          accessToken: 'mocked-access-token',
-          refreshToken: 'mocked-refresh-token',
-        },
+        statusCode: 200,
+        success: true,
       })
     })
 
@@ -69,7 +92,9 @@ describe('AuthController', () => {
           new UnauthorizedException('Incorrect Email or Password')
         )
 
-      await expect(controller.login(dto)).rejects.toThrow(UnauthorizedException)
+      await expect(
+        controller.login(dto, mockResponse as Response)
+      ).rejects.toThrow(UnauthorizedException)
     })
 
     it('should throw UnauthorizedException if password is incorrect', async () => {
@@ -84,7 +109,9 @@ describe('AuthController', () => {
           new UnauthorizedException('Incorrect Email or Password')
         )
 
-      await expect(controller.login(dto)).rejects.toThrow(UnauthorizedException)
+      await expect(
+        controller.login(dto, mockResponse as Response)
+      ).rejects.toThrow(UnauthorizedException)
     })
   })
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards, Res, Req } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { LoginDTO } from './dto/login.dto'
 import { Public } from 'src/common/decorators/public.decorator'
 import { ResponseUtil } from 'src/common/utils/response.util'
+import { RefreshAuthGuard } from './guards/refresh-auth.guard'
+import { Request, Response } from 'express'
 
 @Controller('auth')
 export class AuthController {
@@ -13,7 +15,7 @@ export class AuthController {
 
   @Public()
   @Post('login')
-  async login(@Body() loginDto: LoginDTO) {
+  async login(@Body() loginDto: LoginDTO, @Res() res: Response) {
     const loginResponse = await this.authService.login(loginDto)
 
     return this.responseUtil.response(
@@ -25,5 +27,12 @@ export class AuthController {
         data: loginResponse,
       }
     )
+  }
+
+  @Public()
+  @UseGuards(RefreshAuthGuard)
+  @Post('refresh-token')
+  async refreshToken(@Req() req: Request, @Res() res: Response) {
+    return null
   }
 }
