d8fde0c6b495d9ce9f256e0976688d9c549a9cf5
Muhammad Daffa'I Rafi Prasetyo <88131567+daffarafi@users.noreply.github.com>
2025-03-12 12:43:53 +0700

    fix: Change build script path (#51)
    * fix: methods cors error
    
    * fix: Change build script path
    
    ---------
    
    Co-authored-by: Fathan <fathan.naufal@ui.ac.id>

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
         callback(new Error('Not allowed by CORS'))
       }
     },
-    methods: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   }
   app.enableCors(corsOptions)
   app.use(cookieParser())
