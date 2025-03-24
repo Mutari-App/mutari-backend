09665bc0adbbb0d8a56d491886bf49290d4f6d4b
mma <111668501+mmalkindi@users.noreply.github.com>
2025-03-24 23:45:05 +0700

    feat: add CreateNotificationDto

--- src/notification/dto/create-notification.dto.ts
+++ src/notification/dto/create-notification.dto.ts
@@ -1,1 +1,26 @@
-export class CreateNotificationDto {}
+import { REMINDER_OPTION } from '@prisma/client'
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator'
+
+export class CreateNotificationDto {
+  @IsEmail()
+  recipient: string
+
+  @IsString()
+  recipientName: string
+
+  @IsString()
+  tripName: string
+
+  @IsEnum(REMINDER_OPTION)
+  @IsNotEmpty()
+  reminderOption: 'TEN_MINUTES_BEFORE' | 'ONE_HOUR_BEFORE' | 'ONE_DAY_BEFORE'
+
+  @IsDateString()
+  date: string
+}
