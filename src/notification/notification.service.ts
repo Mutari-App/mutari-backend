e025f4ee6cd63695a12937e2171e263a110193aa
mma <111668501+mmalkindi@users.noreply.github.com>
2025-03-24 23:47:14 +0700

    feat: implement create method in NotificationService

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common'
+import { EmailService } from '../email/email.service'
 import { CreateNotificationDto } from './dto/create-notification.dto'
 import { UpdateNotificationDto } from './dto/update-notification.dto'
+import { SchedulerRegistry } from '@nestjs/schedule'
+import { CronJob } from 'cron'
+import { itineraryReminderTemplate } from './templates/itinerary-reminder-template'
 
 @Injectable()
 export class NotificationService {
+  constructor(
+    private readonly emailService: EmailService,
+    private readonly schedulerRegistry: SchedulerRegistry
+  ) {}
+
   create(createNotificationDto: CreateNotificationDto) {
-    return 'This action adds a new notification'
+    const date = new Date(createNotificationDto.date)
+    const job = new CronJob(date, () => {
+      this.emailService.sendEmail(
+        createNotificationDto.recipient,
+        `Reminder for ${createNotificationDto.tripName}`,
+        itineraryReminderTemplate(
+          createNotificationDto.recipientName,
+          createNotificationDto.tripName,
+          createNotificationDto.reminderOption
+        )
+      )
+    })
+
+    this.schedulerRegistry.addCronJob(
+      `${Date.now()}-${createNotificationDto.recipient}`,
+      job
+    )
+    job.start()
+    return job
   }
 
   findAll() {
