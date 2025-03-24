6183d2cbfb268a101e5e6625fb4e0e361f8b2320
mma <111668501+mmalkindi@users.noreply.github.com>
2025-03-24 23:46:03 +0700

    feat: add module imports and service exports

--- src/itinerary/itinerary.module.ts
+++ src/itinerary/itinerary.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ItineraryService } from './itinerary.service'
 import { ItineraryController } from './itinerary.controller'
+import { NotificationModule } from 'src/notification/notification.module'
 
 @Module({
   controllers: [ItineraryController],
   providers: [ItineraryService],
+  imports: [NotificationModule],
 })
 export class ItineraryModule {}
