99e82acac3bed7c4b732d7f9bb0bddfe4abc62d5
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-24 20:34:08 +0700

    test: create GeminiController unit tests

--- src/gemini/gemini.controller.spec.ts
+++ src/gemini/gemini.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { GeminiController } from './gemini.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { GeminiController } from './gemini.controller'
 
 describe('GeminiController', () => {
-  let controller: GeminiController;
+  let controller: GeminiController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GeminiController],
-    }).compile();
+    }).compile()
 
-    controller = module.get<GeminiController>(GeminiController);
-  });
+    controller = module.get<GeminiController>(GeminiController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
