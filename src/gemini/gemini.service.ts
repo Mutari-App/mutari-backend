7003be53225f3511af1a6afbb7dbf8ff2677cc4e
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-24 20:32:14 +0700

    feat: implement GeminiService with generated API key and generateFeedback function

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai'
+import { Injectable } from '@nestjs/common'
+import { GenerateFeedbackDto } from './model/generate-feedback.dto'
+
+const GEMINI_MODEL = 'gemini-2.0-flash'
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+  private readonly googleAI: GoogleGenerativeAI
+  private readonly model: GenerativeModel
+  constructor(configService: ConfigService) {
+    const geminiApiKey = configService.get('GEMINI_API_KEY')
+    this.googleAI = new GoogleGenerativeAI(geminiApiKey)
+    this.model = this.googleAI.getGenerativeModel({
+      model: GEMINI_MODEL,
+    })
+  }
+
+  async generateFeedback(dto: GenerateFeedbackDto): Promise<string> {
+    try {
+      const input = `Buat feedback dan saran perbaikan untuk itinerary ini, terutama terkait efisiensi waktu, rekomendasi aktivitas, serta pengalaman yang lebih optimal dari itinerary berikut:\n\nPrompt: "${dto.prompt}", mohon berikan beberapa saran perbaikan untuk itinerary ini dalam bentuk poin-poin saja`
+
+      const result = await this.model.generateContent(input)
+      const response = await result.response
+      return response.text()
+    } catch (error) {
+      console.error('Error generating feedback:', error)
+      throw new Error('Failed to generate feedback')
+    }
+  }
+}
