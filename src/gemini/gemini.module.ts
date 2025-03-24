6cf0d98a8bde258c7cd8b5e8b06748d1e1c79579
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-03-24 20:36:56 +0700

    chore: clean GeminiModule

--- src/gemini/gemini.module.ts
+++ src/gemini/gemini.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { GeminiController } from './gemini.controller';
-import { GeminiService } from './gemini.service';
+import { Module } from '@nestjs/common'
+import { GeminiController } from './gemini.controller'
+import { GeminiService } from './gemini.service'
 
 @Module({
   controllers: [GeminiController],
-  providers: [GeminiService]
+  providers: [GeminiService],
 })
 export class GeminiModule {}
