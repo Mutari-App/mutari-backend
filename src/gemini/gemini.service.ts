b1f727535fb1b403dcaad489e7dd3e20750ce21c
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-04-05 05:36:40 +0700

    refactor: remove redundancy

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -95,7 +95,7 @@ export class GeminiService {
     try {
       const prompt = await this.createPrompt(dto.itineraryData)
       const result = await this.model.generateContent(prompt.prompt)
-      const response = await result.response
+      const response = result.response
       const feedbackText = response.text()
 
       let feedbackArray: FeedbackItem[]
