89e87f88539cd766a4cd560f998d86c33655abd8
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-04-05 05:18:41 +0700

    feat: add validation for itineraryData in generate-feedback endpoint

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -7,9 +7,15 @@ import { Public } from 'src/common/decorators/public.decorator'
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
 
-  @Public()
   @Post('generate-feedback')
   async generate(@Body() dto: GenerateFeedbackDto) {
+    if (
+      !dto.itineraryData ||
+      !dto.itineraryData.sections ||
+      dto.itineraryData.sections.length === 0
+    ) {
+      throw new Error('ItineraryData must contain sections.')
+    }
     return this.geminiService.generateFeedback(dto)
   }
 }
