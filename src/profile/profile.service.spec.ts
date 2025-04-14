d51fddf2a87915827ac7e4a68988205e0292f85a
daffarafip24 <daffarafip24@gmail.com>
2025-04-14 18:20:59 +0700

    test: Add test for payment, profile, and ticket from nestjs template

--- src/profile/profile.service.spec.ts
+++ src/profile/profile.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ProfileService } from './profile.service'
+import { CreateProfileDto } from './dto/create-profile.dto'
+import { UpdateProfileDto } from './dto/update-profile.dto'
 
 describe('ProfileService', () => {
   let service: ProfileService
@@ -14,5 +16,44 @@ describe('ProfileService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('create', () => {
+    it('should return a string indicating a new profile has been added', () => {
+      const createProfileDto: CreateProfileDto = {}
+      expect(service.create(createProfileDto)).toBe(
+        'This action adds a new profile'
+      )
+    })
+  })
+
+  describe('findAll', () => {
+    it('should return all profiles', () => {
+      expect(service.findAll()).toBe('This action returns all profile')
+    })
+  })
+
+  describe('findOne', () => {
+    it('should return a profile by id', () => {
+      const id = 1
+      expect(service.findOne(id)).toBe(`This action returns a #${id} profile`)
+    })
+  })
+
+  describe('update', () => {
+    it('should update a profile', () => {
+      const id = 1
+      const updateProfileDto: UpdateProfileDto = {}
+      expect(service.update(id, updateProfileDto)).toBe(
+        `This action updates a #${id} profile`
+      )
+    })
+  })
+
+  describe('remove', () => {
+    it('should remove a profile', () => {
+      const id = 1
+      expect(service.remove(id)).toBe(`This action removes a #${id} profile`)
+    })
   })
 })
--- src/ticket/ticket.service.spec.ts
+++ src/ticket/ticket.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { TicketService } from './ticket.service'
+import { CreateTicketDto } from './dto/create-ticket.dto'
+import { UpdateTicketDto } from './dto/update-ticket.dto'
 
 describe('TicketService', () => {
   let service: TicketService
@@ -14,5 +16,44 @@ describe('TicketService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('create', () => {
+    it('should return a string indicating a new ticket was added', () => {
+      const createTicketDto: CreateTicketDto = {}
+      expect(service.create(createTicketDto)).toBe(
+        'This action adds a new ticket'
+      )
+    })
+  })
+
+  describe('findAll', () => {
+    it('should return a string indicating all tickets are returned', () => {
+      expect(service.findAll()).toBe('This action returns all ticket')
+    })
+  })
+
+  describe('findOne', () => {
+    it('should return a string with the ticket id', () => {
+      const id = 1
+      expect(service.findOne(id)).toBe(`This action returns a #${id} ticket`)
+    })
+  })
+
+  describe('update', () => {
+    it('should return a string with the updated ticket id', () => {
+      const id = 1
+      const updateTicketDto: UpdateTicketDto = {}
+      expect(service.update(id, updateTicketDto)).toBe(
+        `This action updates a #${id} ticket`
+      )
+    })
+  })
+
+  describe('remove', () => {
+    it('should return a string with the removed ticket id', () => {
+      const id = 1
+      expect(service.remove(id)).toBe(`This action removes a #${id} ticket`)
+    })
   })
 })
