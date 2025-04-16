c33875e26b825edb8b10ff657c22cc57376a496f
daffarafip24 <daffarafip24@gmail.com>
2025-04-16 14:45:25 +0700

    refactor: add Public decorator in profile controller

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
 import { ProfileService } from './profile.service'
 import { ResponseUtil } from 'src/common/utils/response.util'
+import { Public } from 'src/common/decorators/public.decorator'
 
 @Controller('profile')
 export class ProfileController {
@@ -9,6 +10,7 @@ export class ProfileController {
     private readonly responseUtil: ResponseUtil
   ) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Get(':id')
   async findOne(@Param('id') id: string) {
