152ed2a30cb06d3d4727f9d7f0c487e24f7e0ff6
Nasya Ayu <ayu.siti21@ui.ac.id>
2025-04-25 15:01:38 +0700

    feat: update parameter publishItinerary method controller

--- src/itinerary/itinerary.controller.ts
+++ src/itinerary/itinerary.controller.ts
@@ -387,10 +387,15 @@ export class ItineraryController {
   }
 
   @Patch(':itineraryId/publish')
-  async publishItinerary(@Param('id') id: string, @GetUser() user: User) {
+  async publishItinerary(
+    @Param('itineraryId') id: string,
+    @GetUser() user: User,
+    @Body('isPublished') isPublished: boolean
+  ) {
     const publishedItinerary = await this.itineraryService.publishItinerary(
       id,
-      user.id
+      user,
+      isPublished
     )
 
     return this.responseUtil.response(
