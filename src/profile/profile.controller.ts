1dfaa7d5b756816217d04c285355cb3c1e3678f0
daffarafip24 <daffarafip24@gmail.com>
2025-04-26 02:03:27 +0700

    feat: modify get profile endpoint, implement getListItineraries and getListItineraryLikes

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -25,4 +25,36 @@ export class ProfileController {
       }
     )
   }
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Get(':id/itineraries')
+  async getListItineraries(@Param('id') id: string) {
+    const itineraries = await this.profileService.getListItineraries(id)
+    return this.responseUtil.response(
+      {
+        message: 'List itinerary retrieved successfully',
+        statusCode: HttpStatus.OK,
+      },
+      {
+        itineraries,
+      }
+    )
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Get(':id/itinerary-likes')
+  async getListItineraryLikes(@Param('id') id: string) {
+    const itineraryLikes = await this.profileService.getListItineraryLikes(id)
+    return this.responseUtil.response(
+      {
+        message: 'List itinerary likes retrieved successfully',
+        statusCode: HttpStatus.OK,
+      },
+      {
+        itineraryLikes,
+      }
+    )
+  }
 }
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -5,6 +5,7 @@ import { PrismaService } from 'src/prisma/prisma.service'
 @Injectable()
 export class ProfileService {
   constructor(private readonly prisma: PrismaService) {}
+
   async findOne(id: string) {
     const user = await this.prisma.user.findUnique({
       where: { id },
@@ -15,10 +16,86 @@ export class ProfileService {
         lastName: true,
         referralCode: true,
         _count: {
-          select: { referrals: true, itineraries: true }, // Menghitung jumlah referrals
+          select: { referrals: true, itineraries: true },
         },
         loyaltyPoints: true,
         itineraries: {
+          select: {
+            _count: {
+              select: { likes: true },
+            },
+          },
+        },
+      },
+    })
+
+    if (!user) throw new NotFoundException(`User with id ${id} not found`)
+
+    const { _count, itineraries, ...userWithoutCount } = user
+
+    const totalLikes = itineraries.reduce((total, itinerary) => {
+      return total + itinerary._count.likes
+    }, 0)
+
+    return {
+      ...userWithoutCount,
+      totalReferrals: _count.referrals,
+      totalItineraries: _count.itineraries,
+      totalLikes,
+    }
+  }
+
+  async getListItineraries(userId: string) {
+    const itineraries = await this.prisma.itinerary.findMany({
+      where: { userId },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        coverImage: true,
+        startDate: true,
+        endDate: true,
+        _count: {
+          select: { likes: true },
+        },
+        sections: {
+          select: {
+            _count: {
+              select: {
+                blocks: {
+                  where: { blockType: BLOCK_TYPE.LOCATION },
+                },
+              },
+            },
+          },
+        },
+      },
+      orderBy: { updatedAt: 'desc' },
+    })
+
+    return itineraries.map((itinerary) => {
+      const totalDestinations = itinerary.sections.reduce((total, section) => {
+        return total + section._count.blocks
+      }, 0)
+
+      const {
+        sections: _sections,
+        _count,
+        ...itineraryWithoutSections
+      } = itinerary
+      return {
+        ...itineraryWithoutSections,
+        totalLikes: _count.likes,
+        totalDestinations,
+      }
+    })
+  }
+
+  async getListItineraryLikes(userId: string) {
+    const itineraryLikes = await this.prisma.itineraryLike.findMany({
+      where: { userId },
+      select: {
+        itinerary: {
           select: {
             id: true,
             title: true,
@@ -27,93 +104,26 @@ export class ProfileService {
             startDate: true,
             endDate: true,
             _count: {
-              select: {
-                likes: true,
-              },
+              select: { likes: true },
             },
             sections: {
               select: {
                 _count: {
                   select: {
                     blocks: {
-                      where: {
-                        blockType: BLOCK_TYPE.LOCATION,
-                      },
+                      where: { blockType: BLOCK_TYPE.LOCATION },
                     },
                   },
                 },
               },
             },
-          },
-          orderBy: {
-            updatedAt: 'desc',
-          },
-        },
-        itineraryLikes: {
-          select: {
-            itinerary: {
-              select: {
-                id: true,
-                title: true,
-                description: true,
-                coverImage: true,
-                startDate: true,
-                endDate: true,
-                _count: {
-                  select: {
-                    likes: true,
-                  },
-                },
-                sections: {
-                  select: {
-                    _count: {
-                      select: {
-                        blocks: {
-                          where: {
-                            blockType: BLOCK_TYPE.LOCATION,
-                          },
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-          orderBy: {
-            createdAt: 'desc',
           },
         },
       },
+      orderBy: { createdAt: 'desc' },
     })
 
-    if (!user) throw new NotFoundException(`User with id ${id} not found`)
-
-    let totalLikes = 0
-    // Calculate total destinations for each itinerary
-    const enrichedItineraries = user.itineraries.map((itinerary) => {
-      const totalDestinations = itinerary.sections.reduce((total, section) => {
-        return total + section._count.blocks
-      }, 0)
-
-      // Remove sections from the returned object
-      const {
-        sections: _sections,
-        _count,
-        ...itineraryWithoutSections
-      } = itinerary
-
-      totalLikes += _count.likes // Accumulate total likes
-
-      return {
-        ...itineraryWithoutSections,
-        totalLikes: _count.likes,
-        totalDestinations,
-      }
-    })
-
-    // Calculate total destinations for each liked itinerary
-    const enrichedLikedItineraries = user.itineraryLikes.map((like) => {
+    return itineraryLikes.map((like) => {
       const totalDestinations = like.itinerary.sections.reduce(
         (total, section) => {
           return total + section._count.blocks
@@ -121,29 +131,16 @@ export class ProfileService {
         0
       )
 
-      // Remove sections from the itinerary object
       const {
         sections: _sections,
         _count,
         ...itineraryWithoutSections
       } = like.itinerary
-
       return {
         ...itineraryWithoutSections,
         totalLikes: _count.likes,
         totalDestinations,
       }
     })
-
-    // Extract _count.referrals as totalReferrals
-    const { _count, ...userWithoutCount } = user
-    return {
-      ...userWithoutCount,
-      totalReferrals: _count.referrals,
-      totalItineraries: _count.itineraries,
-      totalLikes,
-      itineraries: enrichedItineraries,
-      itineraryLikes: enrichedLikedItineraries,
-    }
   }
 }
