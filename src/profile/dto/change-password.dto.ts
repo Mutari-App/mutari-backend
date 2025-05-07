dc6d7583d95a5b2ae42fa26ad699f54ec8cc804e
daffarafip24 <daffarafip24@gmail.com>
2025-05-07 17:15:40 +0700

    refactor: Create custom class validator Match for confirmPassword

--- src/profile/dto/change-password.dto.ts
+++ src/profile/dto/change-password.dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator'
+import { Match } from 'src/common/validators/match.validator'
 
 export class ChangePasswordDto {
   @IsString()
@@ -16,12 +17,8 @@ export class ChangePasswordDto {
   newPassword: string
 
   @IsString()
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @Matches(/^(?=[^A-Z]*[A-Z])/, {
-    message: 'Password must contain at least one uppercase letter',
-  })
-  @Matches(/^(?=.*\d).*$/, {
-    message: 'Password must contain at least one number',
+  @Match('newPassword', {
+    message: 'Confirm password does not match new password',
   })
   confirmPassword: string
 }
